a1bb6710fa079e322d52bfdacc79f41dd8dbee10
Gareth <gar.carey@gmail.com>
2021-08-16 19:14:18 -0400

    initial commit

---
+++ survey.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? Nicknames are also acceptable :) ', (answer1) => {
+  console.log(`Thank you for your valuable feedback: ${answer1}`);
+
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    console.log(`Thank you for your valuable feedback: ${answer1}`);
+
+    rl.question("What do you listen to while doing this? ", (answer3) => {
+      console.log(`Thank you for your valuable feedback: ${answer3}`);
+
+      rl.question("Which meal is your favourite? (eg: dinner, brunch, etc.) ", (answer4) => {
+        console.log(`Thank you for your valuable feedback: ${answer1}`);
+
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          console.log(`Thank you for your valuable feedback: ${answer1}`);
+
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+          console.log(`Thank you for your valuable feedback: ${answer1}`);
+
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              console.log(`Thank you for your valuable feedback: ${answer1}`);
+          
+              console.log(`${answer1} loves to listen to ${answer3} while ${answer2}, devouring ${answer5} for ${answer4},\nprefers ${answer6} over any other sport, and is amazing at ${answer7}.`);
+        
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
